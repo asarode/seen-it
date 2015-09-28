24f4f0d4d6443925a25a290421ec2ad3767cb387
arjun <sarodearjun57@gmail.com>
2015-09-27 23:21:44 -0500

    fix install function

--- skipper.js
+++
@@ -1,8 +0,0 @@
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-  if (msg.action === 'skip') {
-    // Click the next button on the page to move on the to the next image in the
-    // Imgur gallery
-    var nextButton = document.querySelector('.navNext');
-    nextButton.click();
-  }
-});
