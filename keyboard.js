9adada3a0668701dfdaacc3e732a8e21ed441270
Erik Lammers <erik.lammers93@gmail.com>
2015-09-29 12:51:49 -0400

    switch shift to alt
    Realized while typing a post that using shift is an issue.... switched
    to alt.

--- keyboard.js
+++ keyboard.js
@@ -17,12 +17,12 @@ function handleKeyPress(e){
 }
 
 function handleKeyUpDown(e) {
-  if (e.shiftKey && !skipsToggled){ //Skips haven't been toggled yet
+  if (e.altKey && !skipsToggled){ //Skips haven't been toggled yet
     chrome.extension.sendMessage({
       action: 'ToggleSkips'
     });
     skipsToggled = true; //Need to add this or any keypress will toggle skips
-  } else if (!e.shiftKey && skipsToggled) { //Skips have been toggled and shift is no longer being held down.
+  } else if (!e.altKey && skipsToggled) { //Skips have been toggled and shift is no longer being held down.
     chrome.extension.sendMessage({
       action: 'ToggleSkips'
     });
