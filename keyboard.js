5bbe89088070ea4fe7edb0659b395d4226d1df50
Erik Lammers <erik.lammers93@gmail.com>
2015-09-29 13:11:21 -0400

    Disabling the temporary toggle
    I disabled the momentary toggle for now to avoid more short sited
    commits. Switching to alt was a bad call. I'll look into alternatives
    later, but for now I'm just gonna leave it. The toggles (z,x) still work
    and aren't buggy from what I can tell.

--- keyboard.js
+++ keyboard.js
@@ -1,8 +1,8 @@
-var skipsToggled = false;
+//var skipsToggled = false;
 
-window.addEventListener("keydown", handleKeyUpDown, false);
+//window.addEventListener("keydown", handleKeyUpDown, false);
 window.addEventListener("keypress", handleKeyPress, false);
-window.addEventListener("keyup", handleKeyUpDown, false);
+//window.addEventListener("keyup", handleKeyUpDown, false);
 
 function handleKeyPress(e){
   if (e.keyCode == "122") {//Pressed z key 
@@ -16,16 +16,16 @@ function handleKeyPress(e){
   }
 }
 
-function handleKeyUpDown(e) {
-  if (e.altKey && !skipsToggled){ //Skips haven't been toggled yet
-    chrome.extension.sendMessage({
-      action: 'ToggleSkips'
-    });
-    skipsToggled = true; //Need to add this or any keypress will toggle skips
-  } else if (!e.altKey && skipsToggled) { //Skips have been toggled and shift is no longer being held down.
-    chrome.extension.sendMessage({
-      action: 'ToggleSkips'
-    });
-    skipsToggled = false;
-  }
-}
+// function handleKeyUpDown(e) {
+//   if (e.keyCode == "190" && !skipsToggled){ //Skips haven't been toggled yet
+//     chrome.extension.sendMessage({
+//       action: 'ToggleSkips'
+//     });
+//     skipsToggled = true; //Need to add this or any keypress will toggle skips
+//   } else if (e.keyCode == "190" && skipsToggled) { //Skips have been toggled and shift is no longer being held down.
+//     chrome.extension.sendMessage({
+//       action: 'ToggleSkips'
+//     });
+//     skipsToggled = false;
+//   }
+// }
