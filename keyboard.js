841cebf5365352af67dc20f20669363b0b8143c5
Erik Lammers <erik.lammers93@gmail.com>
2015-09-29 14:03:15 -0400

    Changed keys
    Key combos are now Alt+z for toggle skip and Alt+x for toggle storing as
    per suggestion.

--- keyboard.js
+++ keyboard.js
@@ -1,28 +1,27 @@
 //var skipsToggled = false;
 
-//window.addEventListener("keydown", handleKeyUpDown, false);
-window.addEventListener("keypress", handleKeyPress, false);
-//window.addEventListener("keyup", handleKeyUpDown, false);
+//window.addEventListener("keydown", handleKeyDown, false);
+window.addEventListener("keyup", handleKeyUp, false);
 
-function handleKeyPress(e){
-  if (e.keyCode == "122") {//Pressed z key 
+function handleKeyUp(e){
+  if (e.keyCode == "90" && e.altKey) {//Pressed z key 
     chrome.extension.sendMessage({
       action: 'ToggleSkips'
     });
-  } else if (e.keyCode == "120") {//Pressed x key
+  } else if (e.keyCode == "88" && e.altKey) {//Pressed x key
     chrome.extension.sendMessage({
       action: 'ToggleStoring'
     });
   }
 }
 
-// function handleKeyUpDown(e) {
-//   if (e.keyCode == "190" && !skipsToggled){ //Skips haven't been toggled yet
+// function handleKeyDown(e) {
+//   if ((e.keyCode == "37" || e.keyCode == "39") && e.shiftKey && !skipsToggled){ //Skips haven't been toggled yet
 //     chrome.extension.sendMessage({
 //       action: 'ToggleSkips'
 //     });
 //     skipsToggled = true; //Need to add this or any keypress will toggle skips
-//   } else if (e.keyCode == "190" && skipsToggled) { //Skips have been toggled and shift is no longer being held down.
+//   } else if ((e.keyCode == "37" || e.keyCode == "39") && !e.shiftKey && skipsToggled) { //Skips have been toggled and shift is no longer being held down.
 //     chrome.extension.sendMessage({
 //       action: 'ToggleSkips'
 //     });
